365c9c2a5b9cce177a72a3d0a57e20bfbc75ff58
nikitha <spnnh@mail.umkc.edu>
2018-10-07 17:00:33 -0500

    commit

---
+++ ICP_7_Ionic/src/app/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+Fire :  {
+  apiKey: "AIzaSyDgMgxW1YJKJXG5AJm6VPU3rZWdZnXvs2g",
+  authDomain: "healthcare-36fda.firebaseapp.com",
+  databaseURL: "https://healthcare-36fda.firebaseio.com",
+  projectId: "healthcare-36fda",
+  storageBucket: "healthcare-36fda.appspot.com",
+  messagingSenderId: "849602977535"
+}
+};
